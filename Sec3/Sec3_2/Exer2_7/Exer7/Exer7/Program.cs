c9ce0e099f5956e324f4da90441856dff092631b
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 15:42:45 -0300

    Add files via upload

---
+++ Sec3/Sec3_2/Exer2_7/Exer7/Exer7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace Exer7 {
+    class Program {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (x == 0 && y == 0) {
+                Console.WriteLine("Origem");
+            }
+            else {
+                Console.WriteLine("Q4");
+            }
+        }
+    }
+}
---
+++ Sec3/Sec3_2/Exer2_8/Exer8/Exer8/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exer8 {
+    class Program {
+        static void Main(string[] args) { 
+        
+        }
+    }
+}
