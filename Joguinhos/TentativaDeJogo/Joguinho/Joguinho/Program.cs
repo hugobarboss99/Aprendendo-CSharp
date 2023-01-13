bd5b6f3f70c2a6fb4c9cfffa5401b85ab7d96729
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 15:46:58 -0300

    Add files via upload

---
+++ Joguinhos/TentativaDeJogo/Joguinho/Joguinho/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Joguinho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Welcome message
+            Console.WriteLine("Welcome to the game!");
+
+            // Initialize game variables
+            int lives = 3;
+            int score = 0;
+            bool gameOver = false;
+
+            // Game loop
+            while (!gameOver)
+            {
+                // Display current lives and score
+                Console.WriteLine("Lives: " + lives + " Score: " + score);
+
+                // Get player input
+                Console.Write("Enter a command: ");
+                string input = Console.ReadLine();
+
+                // Process player input
+                if (input == "jump")
+                {
+                    Console.WriteLine("You jumped over the obstacle!");
+                    score += 10;
+                }
+                else if (input == "duck")
+                {
+                    Console.WriteLine("You ducked under the obstacle!");
+                    score += 5;
+                }
+                else if (input == "quit")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    gameOver = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command.");
+                    lives--;
+                }
+
+                // Check if player has no more lives
+                if (lives <= 0)
+                {
+                    Console.WriteLine("Game over!");
+                    gameOver = true;
+                }
+            }
+        }
+    }
+}
---
+++ Joguinhos/TentativaDeJogo2/GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Generate a random number between 1 and 100
+            Random random = new Random();
+            int secretNumber = random.Next(1, 101);
+
+            // Initialize game variables
+            int guess;
+            int attempts = 0;
+            bool correct = false;
+
+            // Game loop
+            while (!correct)
+            {
+                // Get player's guess
+                Console.Write("Enter your guess: ");
+                guess = int.Parse(Console.ReadLine());
+
+                // Check if guess is correct
+                if (guess == secretNumber)
+                {
+                    correct = true;
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("Too low. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Too high. Try again.");
+                }
+
+                attempts++;
+            }
+
+            // Display success message
+            Console.WriteLine("Congratulations! You guessed the number in " + attempts + " attempts.");
+        }
+    }
+}
