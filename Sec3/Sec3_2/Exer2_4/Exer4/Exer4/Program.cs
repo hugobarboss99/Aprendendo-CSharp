30f465aff2416a26ad5f0e00182d61234274eb61
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 15:38:35 -0300

    Add files via upload

---
+++ Sec3/Sec3_2/Exer2_4/Exer4/Exer4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Exer4{
+    class program {
+        public static void Main(string[] args) {
+            string[] valores = Console.ReadLine().Split(' ');
+            int HoraInicio = int.Parse(valores[0]);
+            int HoraFinal = int.Parse(valores[1]);
+            int duracao;
+
+            if (HoraInicio < HoraFinal){
+                duracao = HoraFinal - HoraInicio;
+            }
+            else {
+                duracao = 24 - HoraInicio + HoraFinal;
+            }
+
+            Console.WriteLine("O jogo durou " + duracao + " hora(s)");
+        }
+    }
+}
---
+++ Sec3/Sec3_2/Exer2_6/Exer6/Exer6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+namespace Exer6 {
+    class Program {
+        static void Main(string[] args) {
+            double num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if (num < 0.0)
+            {
+                Console.WriteLine("Fora do intervalo");
+            }
+            else if (num > 100.0)
+            {
+                Console.WriteLine("Fora do intervalo");
+            }
+            else if (num >= 0.0 && num <= 25.0) {
+                Console.WriteLine("Intervalo (0, 25]");
+            }
+            else if (num > 25.0 && num <= 50.0)
+            {
+                Console.WriteLine("Intervalo (25, 50]");
+            }
+            else if (num > 50.0 && num <= 75.0)
+            {
+                Console.WriteLine("Intervalo (50, 75]");
+            }
+            else if (num > 75.0 && num <= 100.0)
+            {
+                Console.WriteLine("Intervalo (75, 100]");
+            }
+        }
+    }
+}
