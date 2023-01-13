4278eee9c9e03667fd903b4fc8fc250c08177742
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 15:10:04 -0300

    Add files via upload

---
+++ Exercicio1_6/Exercicio1_6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1_6 {
+    class Program {
+        static void Main(string[] args) {
+            int product1, numproduct1, product2, numproduct2;
+            double price1, price2, totalprice;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            product1 = int.Parse(valores[0]);
+            numproduct1 = int.Parse(valores[1]);
+            price1 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            valores = Console.ReadLine().Split(' ');
+            product2 = int.Parse(valores[0]);
+            numproduct2 = int.Parse(valores[1]);
+            price2 = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            totalprice = price1 * numproduct1 + price2 * numproduct2;
+            Console.WriteLine("Valor a pagar: R$" +totalprice.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
