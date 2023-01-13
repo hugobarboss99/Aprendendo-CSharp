3f7afc5682e237eb323534e23e26c3ece8a9fe5c
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 15:00:19 -0300

    Add files via upload

---
+++ Exercicio1_2/Exercicio1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio1_2 {
+    class Program {
+        static void Main(string[] args) {
+            int A, B, sum;
+            Console.WriteLine("Digite o primeiro numero:");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            B = int.Parse(Console.ReadLine());
+
+            sum = A + B;
+            Console.WriteLine("Resultado da soma dos dois numeros:");
+            Console.WriteLine(sum);
+        
+        }  
+    }
+}
