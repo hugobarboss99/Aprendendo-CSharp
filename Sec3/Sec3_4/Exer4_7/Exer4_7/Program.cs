ecbfd163d0c3ab37bbf328db2d5e723021bee50c
Hugo Barbosa <99156413+hugobarboss99@users.noreply.github.com>
2023-01-13 21:56:20 -0300

    Add files via upload

---
+++ Sec3/Sec3_4/Exer4_7/Exer4_7/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Exer4_7 {
+    class program {
+        static public void Main(string[] args) { 
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++) {
+                int num1 = i;
+                int num2 = i * i;
+                int num3 = i * i * i;
+                Console.WriteLine($"{num1} {num2} {num3}");
+            }
+        }
+    }
+}
